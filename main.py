b28e476a7eff6081513c1770cb78a1a6b09cdeb3
Krishna Rajagopal <krishan.rajagopal@gmail.com>
2023-09-28 16:33:39 -0700

    Added new user data model and user fake data in DB, included the Register form and Login Flask form and added it to the home route

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import json
 from flask import Flask, render_template, request, redirect, url_for, flash,jsonify
+from flask_login import LoginManager,UserMixin,login_user,login_required,logout_user,current_user
 from flask_bootstrap import Bootstrap5
 from faker import Faker
 from faker.providers import DynamicProvider,BaseProvider
 from flask_wtf import FlaskForm
 from wtforms import DecimalField,FloatField
-from wtforms.fields import TimeField, URLField, StringField, SubmitField,SelectField,DateField,IntegerRangeField,IntegerField,TextAreaField,DateField
+from wtforms.fields import TimeField, URLField, StringField, SubmitField,SelectField,DateField,IntegerRangeField,IntegerField,TextAreaField,DateField,EmailField,PasswordField
 from wtforms.validators import DataRequired, URL, NoneOf, NumberRange,Regexp
 from wtforms_validators import AlphaSpace
 import sqlite3
@@ -35,6 +36,17 @@ csrf = CSRFProtect(app)
 db.init_app(app)
 bootstrap = Bootstrap5(app)
 
+#initialize login manager and configure it
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-danger'
+
+#Create User_Loader callback
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, id=user_id)
+
 class Task(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
@@ -44,6 +56,21 @@ class Task(db.Model):
     status = db.Column(db.String(50), unique=False, nullable=False)
     due_date=db.Column(db.Date, unique=False, nullable=False)
     percent_complete = db.Column(db.Integer, nullable=False)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'email': self.email,
+            # Include other attributes as needed
+        }
 
 faker_status=DynamicProvider(
     provider_name="task_status",
@@ -82,6 +109,17 @@ for i in range(0, 30):
 #     print(task)
 # print(tasks)
 
+#use this to create a LIST of Dictionaries of Fake user data
+user = {}
+users=[]
+for i in range(0, 10):
+    user['id'] = i+1
+    user['username'] = fake.name()
+    user['email'] = fake.email()
+    user['password'] = fake.password()
+    users.append(user.copy())
+# print(users)
+
 ##Create Table
 with app.app_context():
     db.create_all()
@@ -91,10 +129,17 @@ with app.app_context():
 
 with app.app_context():
     record_exists = db.session.query(db.session.query(Task).filter_by(id=1).exists()).scalar()
+    user_exists = db.session.query(db.session.query(User).filter_by(id=1).exists()).scalar()
     if not record_exists:
         for task_item in tasks:
             add_new_task_to_db = Task(**task_item)
             db.session.add(add_new_task_to_db)
+            db.session.commit()
+
+    if not user_exists:
+        for user_item in users:
+            add_new_user_to_db = User(**user_item)
+            db.session.add(add_new_user_to_db)
             db.session.commit()
 
 
@@ -107,6 +152,18 @@ class update_task_form(FlaskForm):
     due_date=DateField (label='Due Date', validators=[DataRequired(message="Valid due Date required")])
     percent_complete = IntegerField (label="Percent Complete",validators=[DataRequired(),NumberRange(min=0,max=100,message="Enter a number between 0 and 100")])
     submit = SubmitField(label='Update Task')
+
+class register_form(FlaskForm):
+    firstname = StringField(label='FirstName', validators=[DataRequired()])
+    lastname = StringField(label='LastName', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    email=EmailField(label='Email', validators=[DataRequired()])
+    submit = SubmitField(label='Register')
+
+class login_form(FlaskForm):
+    email=EmailField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Login')
 
 
 # class Delete_task_form(FlaskForm):
@@ -123,9 +180,15 @@ def get_tasks():
 def home():
     form=update_task_form()
     if request.method == 'GET':
-        all_tasks=get_tasks() 
+        all_tasks=Task.query.all()
+        all_users=User.query.all()
         #    print(all_tasks)
-        return render_template('index.html',tasks=list(all_tasks),form=form)
+        # # print(all_users) and all_tasks in json format
+        # tasks_data=[task.serialize() for task in all_tasks]
+        # users_data=[user.serialize() for user in all_users]
+        # print(json.dumps(users_data,indent=4))
+        # print ( f"tasks_json:  {json.dumps(tasks_data,indent=4)}")
+        return render_template('index.html',tasks=list(all_tasks),form=form,login_form=login_form(),register_form=register_form())
     elif  request.method=='POST' and form.validate_on_submit():
         task_id=request.form.get('id')
         task_to_edit = db.get_or_404(Task, task_id)
@@ -149,22 +212,6 @@ def home():
         # return jsonify(data)
 
 
-@app.route('/update_task/<int:id>',  methods=["POST"])
-def update_task(id):
-    all_tasks=get_tasks() 
-    task_to_edit = db.get_or_404(Task, id)
-    # if request.form.validate_on_submit():
-    #     print("Form Fields valid")
-        # task_to_edit.id=request.form.get('id',task_to_edit.id)
-        # task_to_edit.title=request.form.get('title',task_to_edit.title)
-        # task_to_edit.description=request.form.get('description',task_to_edit.description)
-        # task_to_edit.name_assignee=request.form.get('name_assignee',task_to_edit.name_assign)
-        # task_to_edit.status=request.form.get('status',task_to_edit.status)
-        # task_to_edit.due_date=request.form.get('due_date',task_to_edit.due_date)
-        # task_to_edit.percent_complete=request.form.get('percent_complete',task_to_edit.percent_complete)
-        # db.session.commit()
-    return redirect(url_for('home'))
-
 @app.route('/postmethod', methods = ['POST'])
 def post_status_update_data():
     jsdata = request.get_json(force=True,)
@@ -178,6 +225,18 @@ def post_status_update_data():
     
 @app.route("/add", methods=[ "POST"])
 def add():
+    """
+    Add a new task to the database.
+    This function is triggered when a POST request is made to the "/add" endpoint of the API.
+    It retrieves the title, description, name assignee, status, due date, and percent complete
+    from the request form, and then adds a new task object to the database using the SQLAlchemy ORM.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     form=update_task_form()
     if request.method == "POST":
         # print(f"\
@@ -200,35 +259,25 @@ def add():
         flash(message=f"Book Name : {new_task.title} Added successfully", category="alert-success")
         return redirect(url_for('home'))
 
-# @app.route("/task/<int:id>", methods=["GET", "POST"])
-# def update(id):
-#     result = db.session.execute(db.select(Task).order_by(Task.title))
-#     all_db_tasks = result.scalars()
-#     # print(f"Task ID is : id")
-#     # ajax='_ajax' in request.form
-#     form1 = update_task_form()
-#     # task_to_edit = db.get_or_404(Task, id)
-#     # if request.method == "GET":
-#         # form1.id.data                   = task_to_edit.id
-#         # form1.title.data                = task_to_edit.title
-#         # form1.description.data          = task_to_edit.description
-#         # form1.name_assignee.data        = task_to_edit.name_assignee
-#         # form1.status.data               = task_to_edit.status
-#         # form1.due_date.data             = task_to_edit.due_date
-#         # form1.percent_complete.data     = task_to_edit.percent_complete
-#     # if form1.validate_on_submit():
-#     #     book_to_edit.rating = form1.rating.data
-#     #     db.session.commit()
-#     #     flash(f"Rating for book: {book_to_edit.title} updated successfully",category="alert-success")
-#         # if ajax:        # Add these
-#         #     return ''   # two lines
-#     #     return redirect(url_for('home'))
-#     # # print(book_to_edit.title)
-#     # # flash(book_to_edit)
-#     return render_template('update.html', tasks=list(all_db_tasks),form=form1)
 
 @app.route("/delete", methods=["POST"])
 def delete():
+    """
+    Delete a task from the database.
+
+    This function is triggered when a POST request is made to the "/delete" endpoint of the API.
+    It retrieves the ID of the task to be deleted from the request form, and then retrieves the task
+    object from the database using the ID. The task object is then deleted from the database using
+    the SQLAlchemy ORM. Finally, a flash message is displayed to indicate the successful deletion
+    of the task, and the user is redirected to the home page.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+
     # if  request.method=='POST' :
     task_id=request.form.get('id')
     task_to_delete = db.get_or_404(Task, task_id)
